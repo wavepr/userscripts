05c098ecd3724590554fd887c6e21c7c11404500
wavepr <113032511+wavepr@users.noreply.github.com>
2022-09-07 14:49:12 +0200

    Create myanimelist-utilities.user.js

---
+++ myanimelist-utilities.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         MyAnimeList Utilities
+// @namespace    https://github.com/wavepr/userscripts
+// @version      0.1.0
+// @description  MyAnimeList Utilities
+// @author       wavepr
+// @match        https://myanimelist.net/editprofile.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=myanimelist.net
+// @grant        none
+// @downloadURL  https://github.com/wavepr/userscripts/raw/main/myanimelist-utilities.user.js
+// @updateURL    https://github.com/wavepr/userscripts/raw/main/myanimelist-utilities.user.js
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+   
+})();
